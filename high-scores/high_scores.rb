439a74e8cdf9cd2e73b4235acfc98d80aaeeda32
qpzm <lhm1442@gmail.com>
2019-02-09 19:12:14 +0900

    Basic-4 refactor
    1. Replace instance vars with methods
      i.e. @scores -> attr_reader :scores and use scores
    
    2. extract a long logic to a helper method
    
    3. sort -> max(3)

--- high-scores/high_scores.rb
+++ high-scores/high_scores.rb
@@ -1,40 +1,38 @@
 class HighScores
-  attr_accessor :scores
+  attr_reader :scores
 
   def initialize(scores = [])
     @scores = scores
-    @descending = nil
   end
 
   def latest
-    @scores.last
+    scores.last
   end
 
   def personal_best
-    descending!
-    @descending.first
+    personal_top.first
   end
 
   def personal_top
-    descending!
-    @descending.first(3)
+    top3
   end
 
   def report
-    latest_score = latest
-    best_score = personal_best
-
-    "Your latest score was #{latest_score}. " +
-      if latest_score == best_score
-        "That's your personal best!"
-      else
-        "That's #{best_score - latest_score} short of your personal best!"
-      end
+    "Your latest score was #{latest}. " + report_detail
   end
 
   private
 
-  def descending!
-    @descending ||= @scores.sort { |x, y| y <=> x }
+  def top3
+    @top3 ||= scores.max(3)
+  end
+
+  def report_detail
+    diff = personal_best - latest
+    if diff.zero?
+      "That's your personal best!"
+    else
+      "That's #{diff} short of your personal best!"
+    end
   end
 end
