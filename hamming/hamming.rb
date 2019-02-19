a44d5f6ef2cfd46e1e3e4e23133ba0f54c6a5775
qpzm <lhm1442@gmail.com>
2019-02-20 02:50:31 +0900

    Basic-8 hamming

---
+++ hamming/hamming.rb
@@ -0,0 +1,9 @@
+class Hamming
+  def self.compute(str1, str2)
+    raise ArgumentError if str1.length != str2.length
+    str1.each_char.zip(str2.each_char)
+        .map { |e| e[0] == e[1] ? 0 : 1 }
+        .reduce(0, :+)
+  end
+end
+
