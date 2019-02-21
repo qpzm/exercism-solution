f5c6ad45d66803bcee33a7fbe959e59b38fab47e
qpzm <lhm1442@gmail.com>
2019-02-22 01:35:45 +0900

    Basic-9 refactor: use hash and join

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,8 +1,8 @@
 class Raindrops
   def self.convert(num)
-    str = [3, 5, 7].zip(%w(Pling Plang Plong))
-                   .map { |div, word| num % div == 0 ? word : ''}
-                   .reduce(:concat)
+    str = { Pling: 3, Plang: 5, Plong: 7 }
+          .map { |word, div| (num % div).zero? ? word : '' }
+          .join
     str.empty? ? num.to_s : str
   end
 end
