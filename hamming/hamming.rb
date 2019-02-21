efa7b2daafe4bec19c62ca30995fe028542f7c08
qpzm <lhm1442@gmail.com>
2019-02-21 10:09:17 +0900

    Basic-8 refactor2: use Enumerable#count

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,7 +1,6 @@
 class Hamming
   def self.compute(str1, str2)
     raise ArgumentError if str1.length != str2.length
-    str1.each_char.zip(str2.each_char)
-        .count { |a, b| a != b }
+    str1.each_char.with_index.count { |c, i| c != str2[i] }
   end
 end
