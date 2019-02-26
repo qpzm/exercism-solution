703da44412f917b34e600bdb0f71999d54282a31
qpzm <lhm1442@gmail.com>
2019-02-26 16:59:54 +0900

    Basic-11 refactor
    * Remove `include` in `class_eval`
    
    * nil guard through `word || ''`
    
    * reduce -> sum

--- scrabble-score/scrabble_score.rb
+++ scrabble-score/scrabble_score.rb
@@ -1,27 +1,25 @@
-module Scorable
-  def to_score
-    if length != 1
-      raise Exception 'The length of an argument string is not 1 in String#to_score'
-    end
+class Scrabble
+  String.class_eval do
+    def to_score
+      if length != 1
+        raise Exception 'The length of an argument string is not 1 in String#to_score'
+      end
 
-    case self
-    when /[aeioulnrst]/ then 1
-    when /[dg]/ then 2
-    when /[bcmp]/ then 3
-    when /[fhvwy]/ then 4
-    when /[k]/ then 5
-    when /[jx]/ then 8
-    when /[qz]/ then 10
-    else 0
+      case self
+      when /[aeioulnrst]/ then 1
+      when /[dg]/ then 2
+      when /[bcmp]/ then 3
+      when /[fhvwy]/ then 4
+      when /[k]/ then 5
+      when /[jx]/ then 8
+      when /[qz]/ then 10
+      else 0
+      end
     end
   end
-end
-
-class Scrabble
-  String.class_eval { include Scorable }
 
   def initialize(word)
-    @word = word.nil? ? '' : word
+    @word = word || ''
   end
 
   def score
@@ -40,6 +38,6 @@ class Scrabble
     word.downcase
         .each_char
         .map(&:to_score)
-        .reduce(0, :+)
+        .sum
   end
 end
