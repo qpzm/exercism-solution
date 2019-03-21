940f2fcb5e95908fec3d940190cf759d52579306
qpzm <lhm1442@gmail.com>
2019-03-22 03:32:46 +0900

    Basic-12 refactor
      - module -> class
      - make Luhn::valid? as a shortcut for new & valid?
      - each_slice(n) { } -> nil, each_slice(n) -> enumerator

--- luhn/luhn.rb
+++ luhn/luhn.rb
@@ -1,32 +1,36 @@
-module Luhn
-  def self.valid?(num)
-    str = num.gsub(/\s/, '')
-    return false if str.length < 2
-
-    ((checksum str) % 10).zero?
-    rescue StandardError
-      false
+class Luhn
+  def initialize(input)
+    @input = input
   end
-end
 
-def checksum(str)
-  str.each_char
-     .map { |c| to_int c }
-     .reverse
-     .map.with_index { |n, i| double n, i }
-     .map { |n| overflow n }
-     .reduce(:+)
-end
+  def self.valid?(input)
+    luhn = self.new input
+    luhn.valid?
+   end
 
-def to_int(c)
-  if (c.match /[0-9]/) then c.to_i
-  else raise StandardError end
-end
+  def valid?
+    input.gsub!(/\s/, '')
+    return false if input.length < 2 || input.match?(/[^0-9]+/)
 
-def double(n, i)
-  (i % 2).zero? ? n : 2 * n
-end
+    (checksum % 10).zero?
+  end
 
-def overflow(n)
-  n > 9 ? n - 9 : n
+  private
+  attr_accessor :input
+
+  def checksum
+    input.reverse
+         .each_char.map(&:to_i)
+         .each_slice(2).to_a.map { |a, b| double_second a, b }
+         .reduce(:+)
+  end
+
+  def double_second(fst, snd)
+    return fst if snd.nil?
+    fst + overflow(2 * snd)
+  end
+
+  def overflow(n)
+    n > 9 ? n - 9 : n
+  end
 end
