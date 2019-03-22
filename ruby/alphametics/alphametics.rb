19599ab6e771b0d6173a8e9dfb80eb7c0a9a51d0
qpzm <lhm1442@gmail.com>
2019-03-22 20:01:51 +0900

    Create ruby directory

---
+++ ruby/alphametics/alphametics.rb
@@ -0,0 +1,10 @@
+module Alphametics
+  def Alphametics.solve(puzzle)
+    puts 1
+  end
+
+  def parse(puzzle)
+
+  end
+end
+
---
+++ ruby/rail-fence-cipher/rail_fence_cipher.rb
@@ -0,0 +1,13 @@
+module RailFenceCipher
+  def self.encode(str, num_line)
+
+  end
+
+  def self.decode(str, num_line)
+  end
+
+  private
+  def self.create_seq(start, finish, offset)
+  end
+end
+
