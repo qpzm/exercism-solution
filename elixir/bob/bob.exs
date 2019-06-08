9bdbe0ab2d6b2841bac68872e7e2d774f530d18a
qpzm <lhm1442@gmail.com>
2019-06-08 16:49:20 +0900

    Elixir-basic-3 refactor
    Replace regex with string functions
    - String.ends_with?/2
    - Check if there is a letter present through
      `String.upcase(input) != String.downcase(input)`

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,35 +1,31 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      word?(input) and question?(input) -> "Calm down, I know what I'm doing!"
+      shout?(input) and question?(input) -> "Calm down, I know what I'm doing!"
       question?(input) -> "Sure."
       blank?(input) -> "Fine. Be that way!"
-      word?(input) -> "Whoa, chill out!"
+      shout?(input) -> "Whoa, chill out!"
       true -> "Whatever."
     end
   end
 
   defp question?(input) do
-    String.match?(input, ~r/\?\z/)
+    String.ends_with?(input, "?")
   end
 
   defp blank?(input) do
-    String.match?(input, ~r/\A\s*\z/)
+    String.trim(input) == ""
   end
 
-  defp blank?(input) do
-    String.match?(input, ~r/\A\s*\z/)
+  defp shout?(input) do
+    word?(input) and upcase?(input)
   end
 
   defp word?(input) do
-    not num?(input) and upcase?(input)
+    String.upcase(input) != String.downcase(input)
   end
 
   defp upcase?(input) do
-    input == String.upcase(input)
-  end
-
-  defp num?(input) do
-    String.match?(input, ~r/\A[\d,?\s]+\z/)
+    String.upcase(input) == input
   end
 end
