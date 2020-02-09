f4948fbf2e03c9b25d14ac5b1859caa017f8daf3
qpzm <lhm1442@gmail.com>
2020-02-10 03:07:22 +0900

    Ruby-basic-15 Resistor color duo

---
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -0,0 +1,29 @@
+class ResistorColorDuo
+  class << self
+    def value(colors)
+      colors[0..1]
+            .map { |c| color_to_num(c) }
+            .reduce(:+)
+            .to_i
+    end
+
+    private
+
+    def color_to_num(color)
+      COLOR_TO_NUM[color.to_sym]
+    end
+
+    COLOR_TO_NUM = {
+      'black': '0',
+      'brown': '1',
+      'red': '2',
+      'orange': '3',
+      'yellow': '4',
+      'green': '5',
+      'blue': '6',
+      'violet': '7',
+      'grey': '8',
+      'white': '9',
+    }
+  end
+end
