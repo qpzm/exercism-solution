42ff67838569db1825e341546979770fc6105a0f
qpzm <lhm1442@gmail.com>
2021-04-11 05:06:13 +0900

    Haskell-basic-4. Refactor

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -3,4 +3,13 @@ import Data.Char (ord, toLower)
 import Data.List (nub)
 
 isPangram :: String -> Bool
-isPangram = (==) 26 . length . filter (\c -> c `elem` ['a'..'z']) . nub . (map toLower)
+isPangram str = all (>= 1) $ countChar str' zeros
+  where str' = filter (\c -> c `elem` ['a'..'z']) $ map toLower str
+        zeros = take 26 (repeat 0)
+
+countChar :: [Char] -> [Integer] -> [Integer]
+countChar [] counts = counts
+countChar (x : xs) counts = countChar xs (increment counts $ toInteger $ ord x - ord 'a')
+
+increment :: [Integer] -> Integer -> [Integer]
+increment l k = [if i == k then x + 1 else x | (i,x) <- zip [0..] l]
