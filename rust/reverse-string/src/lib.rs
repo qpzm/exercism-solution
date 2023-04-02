ac5aceb12e2cead0bf2fe95f84e64716f966cfb7
qpzm <lhm1442@gmail.com>
2023-04-02 20:29:41 +0900

    Rust-1-1. Use grapheme

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,21 @@
+extern crate unicode_segmentation;
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    return input.chars().rev().collect::<String>();
+    //return input.chars().rev().collect::<String>();
+
+    /*
+    let s = "hello 👋🏽";
+    let count = s.graphemes(true).count(); // 7
+    println!("Number of graphemes: {}", count);
+    */
+
+    input
+        // Split the string into an Iterator of &strs, where each element is an
+        // extended grapheme cluster.
+        .graphemes(true)
+        // Reverse the order of the grapheme iterator.
+        .rev()
+        // Collect all the chars into a new owned String.
+        .collect()
 }
